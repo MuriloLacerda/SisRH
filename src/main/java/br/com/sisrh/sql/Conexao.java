d432741bbe5bb2c530c9a53295a65fcfa6d2af03
MuriloLacerda <muriiloown@gmail.com>
2016-01-19 19:34:40 -0200

    init

---
+++ src/main/java/br/com/sisrh/sql/Conexao.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package br.com.sisrh.sql;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ * 
+ * @author Imaginatio
+ */
+public class Conexao {
+    public Connection getConnection() {
+        try {
+            return DriverManager.getConnection("jdbc:postgresql://localhost:5432/RH", "postgres", "kyoo");
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+        return null;
+    }
+}
